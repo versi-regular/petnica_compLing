913e28ff2a1ead903b8a267795af8e92c2077c6f
Aniko Kovac <poet4lifetime@gmail.com>
2017-06-30 23:01:26 +0200

    Domaći zadatak 1

---
+++ dz1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+DOMAĆI ZADATAK 1:
+
+Izmenite priču tako što ćete poslednju rečenicu obrisati i umesto nje dodati
+vaših nekoliko rečenica kojima ćete promeniti ishod priče.
+
+Nakon toga podelite priču na rečenice funkcijom .split() (Hint: Razmislite koji
+kriterijum za razdvajanje vam je sada potreban umesto dosadašnjeg " "). i
+funkcijom len() proverite koliko rečenica sada ima priči.
+
+"""
+
+priča = "Živele u jednoj bari tri žabe. Svađale su se oko toga koja od njih najbolje peva, čiji je glas najlepši. Njihovoj svađi nikad ne bi došao kraj da nisu odlučile da organizuju takmičenje da se vidi ko je najbolji. To je za stanovnike bare i okoline bio veliki događaj. Sve žabe, i velike i male, su došle da svojim prisustvom pomognu odluku žirija. Ali pred sam početak svi shvatiše da nisu izabrali žiri. Mnogo njih je želelo da odlučuju koja od tri žabe najlepše peva i zato je bila takva galama da niko nikog nije čuo šta govori. Roda, patka i zmija čuju kreketanje (a taj zvuk ih je neodoljivo privlačio) i požure da vide šta se događa. Žabe, ne razmišljajući mnogo, zamole rodu, patku i zmiju da budu članovi žirija. One pristanu i takmičenje poče. Tri žabe su naizmenično pevale sve dok nisu iscrple svoj bogati repertoar i dok nisu promukle. Publika je bila oduševljena. Članovi žirija zamoliše za tišinu i rekoše takmičarkama da priđu bliže da im čestitaju i proglase pobednicu. I žabe priđoše umorne i sretne. Možemo zamisliti šta se dogodilo takmičarkama. A i publici iz prvih redova. Te večeri su roda, patka i zmija imale obilnu večeru. Žabama nije bilo do pesme. Celu noć su razmišljale i šaputale. Nikad više nisu se svađale koja najlepše peva."
