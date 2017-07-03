956f405f6b43ca822786d05ad0598fb03316b539
Aniko Kovac <poet4lifetime@gmail.com>
2017-07-03 14:00:14 +0200

    Domaći zadatak 3

---
+++ dz3.py
@@ -0,0 +1,24 @@
+"""
+DOMAĆI ZADATAK 3:
+
+Pošto smo pokvarili vreme na ovom seminaru i nedostaje nam sunce, ispišite tekst pesme Dodole
+unatrag tako da poslednji red bude prvi, a prvi red poslednji, zatim i da poslednja reč u
+svakom redu bude prva, a prva reč bude poslednja kako bismo oterali kišu i prizvali sunce.
+Za početak slobodno zanemarite poziciju zareza i velika i mala slova.
+
+HINT: Kraj reda označava se sa \n.
+
+HINT: Moje rešenje uključuje 2 for petlje i range() funkciju.
+
+BONUS: Učinite da prvo početno slovo svakog reda u Dodolama unatrag bude veliko, a ostala mala.
+
+BONUS+: Neka i Boga bude velikim slovima kao u originalnoj pesmi.
+
+SUPER-BONUS: Umesto da zarez bude na reči na kojoj je, prebacite je na jednu reč pre u
+Dodolama unatrag.
+npr. umesto:  Bože mili dodole, Oj,
+	 da bude: Bože mili, dodole, oj
+
+"""
+
+dodole = "Oj, dodole, mili Bože\noj, dodo, dodole\nNaša doda Boga moli\noj, dodo, dodole\nSitna kiša da zarosi\noj, dodo, dodole\nNaša polja da potopi\noj, dodo, dodole\nI da rodi bel' pšenica\noj, dodo, dodole\nI kukuruz sa dva klasa\noj, dodo, dodole\nŠto molila umolila\noj, dodo, dodole\nSitna kiša zarosila\noj, dodo, dodole\nNaša polja potopila\noj, dodo, dodole\nI rodila bel' pšenica\noj, dodo, dodole\nI kukuruz sa dva klasa\noj, dodo, dodole"
