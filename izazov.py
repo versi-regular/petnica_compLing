de393b3cd3dd38d8bd32b42e208c107d9ef39281
Aniko Kovac <poet4lifetime@gmail.com>
2018-07-08 19:45:41 +0200

    Petnički Python izazov

---
+++ izazov.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+PETNIČKI PYTHON IZAZOV
+
+LVL1 - Petnički pastir:
+* Izbrojte ukupan broj reči u pesmi Devojka iz Petnice.
+* Izbrojte broj različitih reči u pesmi.
+
+LVL2 - Vešti Vebster:
+* Napravite rečnik svih reči iz pesme uz podatak o učestalosti njihovog javljanja.
+* Ispišite lepo formatiran rečnik i frekvence reči.
+  REČ     FREKVENCA
+  reč1    frekvenca1
+  reč2    fekvenca2
+  ...
+* BONUS: Neka ispisani rečnik bude poređan po abecedi.
+  HINT: http://python-reference.readthedocs.io/en/latest/docs/functions/sorted.html
+
+LVL3 - Kraljevski krotitelj zmija:
+* Načinite potrebne promene kako biste stvorili pesmu Dečak iz Petnice i ispišite tu pesmu.
+  HINT: http://python-reference.readthedocs.io/en/latest/docs/str/endswith.html
+
+Imate pravo za jedan dodatan hint na svakih 7 minuta. Pošaljite pitanje u Viber grupi.
+
+"""
+
+devojka_iz_petnice = "Da je ljubav nauka\ndobio bih Nobela\nA da je ljubav knjiga\nuzeo bih NIN-a\n\nDevojke iz Petnice\nredom su pametnice\nA jedna među njima\nsad moje srce ima\n\nŽelim da sam projekat\ni da sam joj u mislima\nmrtvi Vizantinac\niz srednjeg veka pisac\nželim da sam bitan\nu njenim poslovima\n\nDevojka iz Petnice\nona meni znači sve\ni kada me se seti\nja hoću da poletim\n\nDevojka iz Petnice\nu koju sam zaljubljen\nsad uči tamo negde\nDa li pamti mene?\n\nHoću da sam tema\nU nečemu što sprema\nMrtvi Vizantinac\niz srednjeg veka pisac\nželim da sam bitan\nu njenim poslovima﻿"
